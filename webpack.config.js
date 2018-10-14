aeee16c37a1ad8ddd82d8e67d32e9073d126f799
Olaolu Olawuyi <mrolaolu@gmail.com>
2018-10-14 23:17:27 +0100

    🦄: fix webpack compatibility issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-	entry: './main.js',
-	output: {
-		path: __dirname,
-		filename: 'dist/index.js'
-	},
-	devServer:{
-		port:4000,
-		inline: true
-	},
-	devtool: 'inline-source-map',
-	module:{
-		loaders: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: 'babel-loader',
-			query: {
-				presets: ['stage-2', 'react', 'es2015']
-			},
-			
-		}]
-	}
-}
+  entry: './main.js',
+  output: {
+    path: __dirname,
+    filename: 'dist/index.js'
+  },
+  devServer: {
+    port: 4000,
+    inline: true
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['stage-2', 'react', 'es2015']
+        }
+      }
+    ]
+  }
+};
