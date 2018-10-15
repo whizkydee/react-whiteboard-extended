e39b111163c2b6b8d049b38757a0f8a2cf80946b
Olaolu Olawuyi <mrolaolu@gmail.com>
2018-10-15 01:45:08 +0100

    Updates

---
+++ precache-manifest.c129c220bbc93534c99f5e39f3aadb91.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b30c3ef35f2483dfb80",
+    "url": "/react-whiteboard-extended/static/js/runtime~main.3b30c3ef.js"
+  },
+  {
+    "revision": "54507d2da66addce1a2d",
+    "url": "/react-whiteboard-extended/static/js/main.54507d2d.chunk.js"
+  },
+  {
+    "revision": "c77c3cad6a2af8fdb477",
+    "url": "/react-whiteboard-extended/static/js/1.c77c3cad.chunk.js"
+  },
+  {
+    "revision": "584852c64456473207b8d39fe1961dab",
+    "url": "/react-whiteboard-extended/index.html"
+  }
+];
